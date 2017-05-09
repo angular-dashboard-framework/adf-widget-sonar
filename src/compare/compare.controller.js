a0f7de29d32408d3c08933e155f584b11dd499bc
Michael Behlendorf <michael.behlendorf@cloudogu.com>
2017-05-09 10:39:42 +0200

    #8 implement typeahead for project compare

--- src/compare/compare.controller.js
+++ src/compare/compare.controller.js
@@ -1,7 +1,6 @@
 'use strict';
 
-sonarADFWidget.
-controller('compare', compare);
+sonarADFWidget.controller('compare', compare);
 
 function compare(data) {
   var vm = this;
@@ -9,4 +8,29 @@ function compare(data) {
   vm.projectLeft = data.projectLeft;
   vm.projectRight = data.projectRight;
 
+}
+
+sonarADFWidget.controller('editController', editController);
+
+function editController($scope, $http, sonarApi, sonarEndpoint) {
+  var vm = this;
+
+  $scope.updateProjects = function() {
+    var url;
+    if ($scope.config.apiUrl) {
+      url = $scope.config.apiUrl;
+    } else {
+      url = sonarEndpoint.url;
+    }
+    vm.projects = [];
+    sonarApi.getProjects(url).then(function(data) {
+      data.forEach(function(project) {
+        var proj = {
+          name: project.k
+        }
+        vm.projects.push(proj);
+      });
+    });
+  }
+  $scope.updateProjects();
 }
