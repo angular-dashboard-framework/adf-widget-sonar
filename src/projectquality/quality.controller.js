b081a9ae4dbceb32766d9915bb2af6b7b3596123
Christoph Wolfes <christoph.wolfes@triology.de>
2019-03-11 16:50:20 +0100

    fixes small bug

--- src/projectquality/quality.controller.js
+++ src/projectquality/quality.controller.js
@@ -4,23 +4,24 @@ sonarADFWidget.controller('qualityCtrl', qualityCtrl);
 
 function qualityCtrl(data) {
     var vm = this;
-    vm.project = data.project.split(':')[1];;
-    vm.url = data.url;
+    if(data!="Please Setup the Widget") {
 
+      vm.project = data.project.split(':')[1];
+      vm.url = data.url;
 
-    angular.forEach(data.quality_index, function (metric) {
+      angular.forEach(data.quality_index, function (metric) {
         if (metric.metric === "coverage") //going through all entries with if/elseif since there could miss some entries. So there is no special order
-            vm.coverage = metric.value;
+          vm.coverage = metric.value;
         else if (metric.metric === "blocker_violations")
-            vm.blocker = metric.value;
+          vm.blocker = metric.value;
         else if (metric.metric === "alert_status") {
-            vm.qualityGateStatus = metric.value;
-        }
-        else if (metric.metric === "sqale_index") {
+          vm.qualityGateStatus = metric.value;
+        } else if (metric.metric === "sqale_index") {
           vm.technicalDept = metric.value;
-        }
-        else if (metric.metric === "vulnerabilities") {
+        } else if (metric.metric === "vulnerabilities") {
           vm.vulnerabilities = metric.value;
         }
-    });
+      });
+    }
+
 }
